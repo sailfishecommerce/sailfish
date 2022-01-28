32bb3b4d87a3e2e35fa6e2e493f4078f924a9189
BisiDavi <oludavidconnect@gmail.com>
2022-01-28 09:50:26 +0100

    airwallex payment updated

--- components/AirwallexDropin.tsx
+++ components/AirwallexDropin.tsx
@@ -10,7 +10,7 @@ import {
 import { useRouter } from "next/router";
 import { toast } from "react-toastify";
 
-import { useToast, useLoading } from "@/hooks";
+import SpinnerRipple from "@/components/spinnerLoader";
 
 interface AirwallexDropinProps {
   intent_id: string | any;
@@ -24,10 +24,14 @@ export default function AirwallexCard({
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
   const router = useRouter();
-  const { loading, updateLoadingState } = useLoading();
-  const { isLoading, isSuccessful, hasError } = useToast();
 
   console.log("intent_id", intent_id, "client_secret", client_secret);
+
+  useEffect(() => {
+    if (isSubmitting) {
+      toast.loading("Processing");
+    }
+  }, [isSubmitting]);
 
   useEffect(() => {
     loadAirwallex({
@@ -52,6 +56,7 @@ export default function AirwallexCard({
     const onError = (event: CustomEvent): void => {
       const { error } = event.detail;
       setIsSubmitting(false);
+      toast.error(error);
       setErrorMessage(error.message ?? JSON.stringify(error));
       console.error("There is an error", error);
     };
@@ -65,15 +70,13 @@ export default function AirwallexCard({
   }, []);
 
   useEffect(() => {
-    if (loading && !isSubmitting && errorMessage.length > 0) {
-      updateLoadingState();
+    if (!isSubmitting && errorMessage.length > 0) {
       toast.error(errorMessage);
     }
-  }, [errorMessage, isSubmitting, loading]);
+  }, [errorMessage, isSubmitting]);
 
   const triggerConfirm = (): void => {
     setIsSubmitting(true);
-    const loading = isLoading();
     setErrorMessage("");
     const card = getElement("card");
     if (card) {
@@ -90,7 +93,7 @@ export default function AirwallexCard({
         .then((response) => {
           console.log("response triggerConfirm", response);
           setIsSubmitting(false);
-          isSuccessful(loading, "Payment successful");
+          toast.success("Payment successful");
           window.alert(
             `Payment Intent confirmation was successful: ${JSON.stringify(
               response
@@ -102,7 +105,7 @@ export default function AirwallexCard({
           setIsSubmitting(false);
           setErrorMessage(error.message ?? JSON.stringify(error));
           console.error("There is an error", error);
-          hasError(loading, error);
+          toast.error(error);
         });
     }
   };
@@ -116,7 +119,7 @@ export default function AirwallexCard({
 
   return (
     <div>
-      {!elementShow && <p>Loading...</p>}
+      {!elementShow && <SpinnerRipple />}
       {errorMessage.length > 0 && (
         <p
           className="alert bg-danger text-white text-center fw-bold"
